5cf0f1b782ac2da764e1dda1165e1b667366bec3
phanchaunguyen <phanchaunguyen4@gmail.com>
2026-04-08 01:24:16 +0700

    fix ddb connection

--- backend/src/main/java/com/hcmut/backend/repository/UserRepository.java
+++ backend/src/main/java/com/hcmut/backend/repository/UserRepository.java
@@ -7,6 +7,6 @@ import java.util.UUID;
 
 
 public interface UserRepository extends JpaRepository<User, UUID>{
-    Optional<User> findByUserName(String username);
+    Optional<User> findByUsername(String username);
     
 }
