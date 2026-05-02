26983c1dcb878883b4570bd7a823eb5dc31fe4e1
phanchaunguyen <phanchaunguyen4@gmail.com>
2026-05-02 20:24:03 +0700

    UPDATE: Schema update to handle room and group

--- backend/src/main/java/com/hcmut/backend/model/Device.java
+++ backend/src/main/java/com/hcmut/backend/model/Device.java
@@ -12,9 +12,19 @@ public class Device {
     @Column(unique = true, name = "device_mac_address")
     private String macAddress;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "room_id")
+    private Room room;
+
     @Column(name = "is_active")
     private boolean isActive = false;
 
     @Column(name = "current_user_id")
     private String currentUser;
+
+    @Column(name = "x_position")
+    private Integer xPosition;
+
+    @Column(name = "y_position")
+    private Integer yPosition;
 }
--- backend/src/main/java/com/hcmut/backend/model/Room.java
+++ backend/src/main/java/com/hcmut/backend/model/Room.java
@@ -1,4 +1,23 @@
 package com.hcmut.backend.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import java.util.UUID;
+
+@Data
+@Entity
+@Table(name = "rooms")
 public class Room {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(name = "room_code", unique = true, nullable = false, length = 10)
+    private String roomCode;
+
+    private Integer capacity;
 }
