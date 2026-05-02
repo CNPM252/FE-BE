61ba32fea2b0c767f013ce32c5ac22aa0dcaebc9
phanchaunguyen <phanchaunguyen4@gmail.com>
2026-05-02 21:19:18 +0700

    ADD: Group Object

--- backend/src/main/java/com/hcmut/backend/controller/DeviceController.java
+++ backend/src/main/java/com/hcmut/backend/controller/DeviceController.java
@@ -1,12 +1,13 @@
 package com.hcmut.backend.controller;
 
+import com.hcmut.backend.dto.DeviceJoinRequest;
 import com.hcmut.backend.model.Device;
 import com.hcmut.backend.service.DeviceService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/devices")
@@ -35,4 +36,41 @@ public class DeviceController {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
     }
+
+
+    /// /////////////////////////////////
+    /// Gắn máy và x,y vào room
+    /// ////////////////////////////////
+
+
+    @PostMapping("/auto-join")
+    public ResponseEntity<?> autoJoin(@RequestBody DeviceJoinRequest request) {
+        try {
+            Device savedDevice = deviceService.autoJoinRoom(request);
+            return ResponseEntity.ok("Thiết bị " + savedDevice.getMacAddress() + " đã vào phòng thành công!");
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PostMapping("/manual-join")
+    public ResponseEntity<?> manualJoin(@RequestBody DeviceJoinRequest request) {
+        try {
+            Device savedDevice = deviceService.manuallyAssignRoom(request);
+            return ResponseEntity.ok(savedDevice);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PutMapping("/{macAddress}/position")
+    public ResponseEntity<?> updatePosition(@PathVariable String macAddress, @RequestBody Map<String, Integer> position) {
+        try {
+            Device updated = deviceService.updatePosition(macAddress, position.get("x"), position.get("y"));
+            return ResponseEntity.ok(updated);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
 }
